c2ea352e0fda062a4833b501641cc19d03f9ae54
Patrick Keane <pkeane92@yahoo.com>
2018-10-11 22:23:23 -0500

    hit a roadblock

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+var score = 0;
+var magicnumber = Math.floor(Math.random()*120) + 19;
+var winnum = $("#win-number").text;
+var lossnum = $("#loss-number").text;
+
+function restart() {
+    score = 0;
+    console.log(score);
+    magicnumber = Math.floor(Math.random()*120) + 19;
+    console.log(magicnumber);
+    var bluenumber = Math.floor(Math.random()*12) + 1;
+    console.log(bluenumber);
+    $("#blue").attr("data-points", bluenumber);
+    console.log($("#blue").attr("data-points"));
+    $("#red").data("points", Math.floor(Math.random()*12) + 1);
+    $("#green").data("points", Math.floor(Math.random()*12) + 1);
+    $("#orange").data("points", Math.floor(Math.random()*12) + 1);
+    }
+
+restart();
+    
+$(".crystal").on("click", function() {
+    var pointval = ($(this).attr("data-points"));
+    console.log(pointval);
+    pointval = parseInt(pointval);
+    score += pointval;
+    $("#score").text = score;
+
+    if (score = magicnumber) {
+    winnum = winnum++;
+    $("#win-number").text = winnum;
+    alert("You win!")
+    restart();
+    }
+
+    else if (score > magicnumber) {
+    lossnum = lossnum++;
+    $("#loss-number").text = lossnum;
+    alert("You lose! Loser!")
+    restart();
+    }
+})
+
