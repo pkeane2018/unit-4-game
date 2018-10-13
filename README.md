6d10921b29455df0ae7f78d06c07c62813016227
Patrick Keane <pkeane92@yahoo.com>
2018-10-13 18:25:13 -0500

    pretty much finished and edited the Readme file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # unit-4-game
+I created a simple game in html, with a linked css file to format the page and a javascript file to write dynamic programs, which allow the game to run. The game generates a random target number and then sets the attribute of each crystal image to a random number. By clicking on each picture, the number value of the attribute of that image is added to that player's score. The goal is to match the score to the target number. If this happens, the player wins. If the score becomes greater than the target value, the player loses. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,67 @@
+$(document).ready(function() {
+
 var score = 0;
+var scorenum = 0;
 var magicnumber = Math.floor(Math.random()*120) + 19;
-var winnum = $("#win-number").text;
-var lossnum = $("#loss-number").text;
+$(".magic-number").text(magicnumber);
+console.log(magicnumber);
+var winnum = 0;
+var lossnum = 0;
+var bluenumber = Math.floor(Math.random()*12) + 1;
+$("#blue").attr("points", bluenumber);
+var rednumber = Math.floor(Math.random()*12) + 1;
+$("#red").attr("points", rednumber);
+var greennumber = Math.floor(Math.random()*12) + 1;
+$("#green").attr("points", greennumber);
+var orangenumber = Math.floor(Math.random()*12) + 1;
+$("#orange").attr("points", orangenumber);
 
 function restart() {
     score = 0;
+    scorenum = 0;
     console.log(score);
     magicnumber = Math.floor(Math.random()*120) + 19;
     console.log(magicnumber);
-    var bluenumber = Math.floor(Math.random()*12) + 1;
-    console.log(bluenumber);
+    $(".magic-number").text(magicnumber);
+    bluenumber = Math.floor(Math.random()*12) + 1;
     $("#blue").attr("points", bluenumber);
-    console.log($("#blue").attr("points"));
-    $("#red").data("points", Math.floor(Math.random()*12) + 1);
-    $("#green").data("points", Math.floor(Math.random()*12) + 1);
-    $("#orange").data("points", Math.floor(Math.random()*12) + 1);
+    rednumber = Math.floor(Math.random()*12) + 1;
+    $("#red").attr("points", rednumber);
+    greennumber = Math.floor(Math.random()*12) + 1;
+    $("#green").attr("points", greennumber);
+    orangenumber = Math.floor(Math.random()*12) + 1;
+    $("#orange").attr("points", orangenumber);
+    $("#score").text(score);
     }
-
-restart();
     
 $(".crystal").on("click", function() {
-    var pointval = ($(this).attr("data-points"));
+    var pointval = ($(this).attr("points"));
     console.log(pointval);
     pointval = parseInt(pointval);
-    score += pointval;
-    $("#score").text = score;
+    scorenum += pointval;
+    score = scorenum.toString();
+    console.log(score);
+    $("#score").text(score);
 
-    if (score = magicnumber) {
-    winnum = winnum++;
-    $("#win-number").text = winnum;
+    if (score == magicnumber) {
+    winnum += 1;
+    console.log(winnum);
+    $("#win-number").text(winnum);
     alert("You win!")
     restart();
     }
 
     else if (score > magicnumber) {
-    lossnum = lossnum++;
-    $("#loss-number").text = lossnum;
+    lossnum += 1;
+    console.log(lossnum);
+    $("#loss-number").text(lossnum);
     alert("You lose! Loser!")
     restart();
     }
 })
+
+
+})
+
+
 
